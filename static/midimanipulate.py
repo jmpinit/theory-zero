bfc83f5fc7bbfcab34b3d241dcdab3faa5e8b2c6
Ulysses Popple <ulysses.popple@gmail.com>
2015-09-27 09:04:37 -0400

    Create valid json file

--- static/midimanipulate.py
+++ static/midimanipulate.py
@@ -1,16 +1,20 @@
 import midi
 import re
 
+enableLogging = False;
+
 pattern = midi.read_midifile("song.mid")
 
 regex = re.compile('resolution=([0-9]*)')
 resolution = int(regex.findall(str(pattern))[0])
-print "resolution = " + str(resolution)
+if enableLogging:
+    print "resolution = " + str(resolution)
 #resolution is pulses per quarter note or ticks per beat
 
 
 regex = re.compile('SetTempoEvent\(tick=([0-9]*), data=\[([0-9]*), ([0-9]*), ([0-9]*)')
-print regex.findall(str(pattern))
+if enableLogging:
+    print regex.findall(str(pattern))
 temp = regex.findall(str(pattern))[0]
 
 uSeconds = ((int(temp[1])) << 2*8) | ((int(temp[2])) << 1*8) | ((int(temp[3])) << 0*8)
@@ -18,33 +22,45 @@ uSeconds = ((int(temp[1])) << 2*8) | ((int(temp[2])) << 1*8) | ((int(temp[3])) <
 
 #us/quarternote * 1 / ticks/quarternote = uS/tick
 
-msPerTick = uSeconds / (resolution*1000.0) 
+msPerTick = uSeconds / (resolution*1000.0)
 
-print "ms per tick = " + str(msPerTick)
+if enableLogging:
+    print "ms per tick = " + str(msPerTick)
 
- 
+
 regex = re.compile('Note([A-Za-z]*)Event\(tick=([0-9]*), channel=([0-9]*), data=\[([0-9]*)')
 temp = regex.findall(str(pattern))
 temp.sort(key=lambda tup: tup[2])
-print temp
+if enableLogging:
+    print temp
 
-#outputJson = '{ "midi": { "channel": 1, "notes": [{"t":30,"n":58}, {"t":30,"n":58}]';
-outputJson = '{ "midi": '
+#outputJson = '{ "midi": { "channels" : [ "channel": 1, "notes": [{"t":30,"n":58}, {"t":30,"n":58}]]}}';
+notes = []
 
-print temp[len(temp)-1]
+if enableLogging:
+    print temp[len(temp)-1]
 for i in range(1, int(temp[len(temp)-1][2])+1):
-    outputJson = outputJson + '{ "channel": ' + str(i) + ', "notes": ['
+    # outputJson = outputJson + '{ "channel": ' + str(i) + ', "notes": ['
     curTime = 0.0
     #i is channel
     for item in temp:
-        print "item: " + str(item)
+        if enableLogging:
+            print "item: " + str(item)
         if (int(item[2]) == i):
             curTime = curTime + int(item[1])*msPerTick
             if (item[0] == "On"):
-                outputJson = outputJson + '{"t":' + str(curTime) + ', "n":' + item[3] + '},' 
-    outputJson = outputJson[:-1]
-    outputJson = outputJson + ']}, '    
-outputJson = outputJson[:-2]
-outputJson = outputJson + '}'    
-print "---------------------------------------------------"
+                notes.append((curTime, item[3]));
+    # outputJson = outputJson + ']}, '
+
+notes = sorted(notes, key=lambda note: note[0]);
+
+outputJson = '{ "midi": ['
+
+for note in notes:
+    outputJson = outputJson + '{"t":' + str(note[0]) + ', "n":' + note[1] + '},'
+
+outputJson = outputJson[:-1]
+outputJson = outputJson + ']}'
+if enableLogging:
+    print "---------------------------------------------------"
 print outputJson
